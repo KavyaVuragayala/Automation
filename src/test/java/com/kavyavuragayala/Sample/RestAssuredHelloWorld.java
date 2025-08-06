931d4b6c11d68413f1bf0a833ea551179bc5c85a
KavyaVuragayala <vura.kavya18@gmail.com>
2025-08-06 20:25:52 +0530

    commit

---
+++ src/test/java/com/kavyavuragayala/Sample/RestAssuredHelloWorld.java
@@ -0,0 +1,30 @@
+package com.kavyavuragayala.Sample;
+
+import io.restassured.RestAssured;
+
+public class RestAssuredHelloWorld {
+    public static void main(String[] args) {
+
+        // checking for restful booker ping health uri
+        // https://restful-booker.herokuapp.com/ping
+
+
+        // RestAssured follows Gherkin Syntax
+        // given()  --> pre request -> url,headers,auth,body....
+        // .when()  --> HTTP methods
+        // .then()  --> validation 
+
+
+        RestAssured.given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/ping")
+
+                .when()
+                    .log().all().get()
+                    // without log().all() we dont see any logs in the output
+
+                .then()
+                    .log().all().statusCode(201);
+
+    }
+}
